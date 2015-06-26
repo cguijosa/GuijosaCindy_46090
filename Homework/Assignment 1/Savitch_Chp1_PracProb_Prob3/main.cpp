f3d43b62c3a4e975d087fd9a7be24131696f68ac
cguijosa <cindy_guijosa@yahoo.com>
2015-06-26 17:37:12 -0400

    Signed-off-by: cguijosa <cindy_guijosa@yahoo.com>

---
+++ Homework/Assignment 1/Savitch_Chp1_PracProb_Prob3/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Cindy Guijosa
+ * Purpose: Homework
+ * Created on June 26, 2015, 5:32 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    //Declare variables
+    int numPods,peaPPod, totPeas;
+    
+    //Output and inputs
+    cout<<"Press return after entering a number.\n";
+    cout<<"Enter the number of pods:\n";
+    
+    cin>>numPods;//The user inputs the number of pods
+    
+    cout<<"Enter the number of peas in a pod:\n";
+    cin>>peaPPod;//The user inputs the amount of peas per pod
+    totPeas=numPods/peaPPod;//Total amount of peas. Division to show error message will show
+    cout<<"If you have ";
+    cout<<numPods;
+    cout<<" pea pods\n";
+    cout<<"and ";
+    cout<<peaPPod;
+    cout<<" peas in each pod, then\n";
+    cout<<"you have ";
+    cout<<totPeas;
+    cout<<" peas in all the pods.\n";
+           
+    return 0;
+}
+
